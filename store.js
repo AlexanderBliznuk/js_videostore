9e75d88f6baad64aab7d7cd3bbee899ebc67838d
Alexander <alexander.bliznuk@yandex.ru>
2016-09-26 16:19:51 +0300

    added getMovieCost function; replaced code with its calls;

--- store.js
+++ store.js
@@ -1,73 +1,79 @@
 "use strict";
 
 function statement(customer, movies) {
-  let totalCost = 0;
-  let totalFrequentRenterPoints = 0;
-  let result = `Rental Record for ${customer.name}\n`;
-  for (let rental of customer.rentals) {
-    let movie = findMovie(rental);
-    let currentMovieCost = 0;
+    let totalCost = 0;
+    let totalFrequentRenterPoints = 0;
+    let result = `Rental Record for ${customer.name}\n`;
+    for (let rental of customer.rentals) {
+        let movie = findMovie(rental);
+        let currentMovieCost = getMovieCost(rental);
 
-    // determine amount for each movie
-    switch (movie.type) {
-      case "regular":
-        currentMovieCost = 2;
-        if (rental.days > 2) {
-          currentMovieCost += (rental.days - 2) * 1.5;
-        }
-        break;
-      case "new":
-        currentMovieCost = rental.days * 3;
-        break;
-      case "children":
-        currentMovieCost = 1.5;
-        if (rental.days > 3) {
-          currentMovieCost += (rental.days - 3) * 1.5;
-        }
-        break;
+        //add frequent renter points
+        totalFrequentRenterPoints++;
+        // add bonus for a two day new release rental
+        if (movie.type === "new" && rental.days > 2) totalFrequentRenterPoints++;
+
+        //print figures for this rental
+        result += `\t${movie.title}\t${currentMovieCost}\n`;
+        totalCost += currentMovieCost;
+    }
+    // add footer lines
+    result += `Amount owed is ${totalCost}\n`;
+    result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
+
+    return result;
+
+    function findMovie(rental) {
+        return movies[rental.movieID];
     }
 
-    //add frequent renter points
-    totalFrequentRenterPoints++;
-    // add bonus for a two day new release rental
-    if (movie.type === "new" && rental.days > 2) totalFrequentRenterPoints++;
+    function getMovieCost(rental) {
+        let cost = 0;
+        let movie = findMovie(rental);
 
-    //print figures for this rental
-    result += `\t${movie.title}\t${currentMovieCost}\n`;
-    totalCost += currentMovieCost;
-  }
-  // add footer lines
-  result += `Amount owed is ${totalCost}\n`;
-  result += `You earned ${totalFrequentRenterPoints} frequent renter points\n`;
-
-  return result;
-
-  function findMovie(rental) {
-    return movies[rental.movieID];
-  }
+        // determine amount for each movie
+        switch (movie.type) {
+            case "regular":
+                cost = 2;
+                if (rental.days > 2) {
+                    cost += (rental.days - 2) * 1.5;
+                }
+                break;
+            case "new":
+                cost = rental.days * 3;
+                break;
+            case "children":
+                cost = 1.5;
+                if (rental.days > 3) {
+                    cost += (rental.days - 3) * 1.5;
+                }
+                break;
+        }
+        return cost;
+    }
 }
 
 let customer = {
-  name: "martin",
-  rentals: [{
-    "movieID": "F001",
-    "days": 3
-  }, {
-    "movieID": "F002",
-    "days": 1
-  }, ]
+    name: "martin",
+    rentals: [{
+        "movieID": "F001",
+        "days": 3
+    }, {
+        "movieID": "F002",
+        "days": 1
+    }, ]
 };
 
 let movies = {
-  "F001": {
-    "title": "Ran",
-    "type": "regular"
-  },
-  "F002": {
-    "title": "Trois Couleurs: Bleu",
-    "type": "regular"
-  },
-  // etc
+    "F001": {
+        "title": "Ran",
+        "type": "regular"
+    },
+    "F002": {
+        "title": "Trois Couleurs: Bleu",
+        "type": "regular"
+    },
+    // etc
 };
 
 console.log(statement(customer, movies));
